aac763085eacfe522e81e6717bbc80c2fb0ef077
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-15 11:16:39 +0100

    Refactoring the tests

--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -5,4 +5,9 @@ describe 'make_snippet' do
     first_five = make_snippet("In this project you will build personal diary system")
     expect(first_five).to eq "In this project you will..."
   end
+
+  it 'Show all the words if the string has less than five words' do
+    all_words = make_snippet("I love Ruby")
+    expect(all_words).to eq "I love Ruby..."
+  end
 end
